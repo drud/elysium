6b98864a0319187e12584d6ab9b6c42959d69c3f
Brad Bowman <beeradb@gmail.com>
2017-05-23 14:20:43 -0600

    [master] Initial import of the elysium package. Supports sessions + site lists

---
+++ pkg/api/session.go
@@ -0,0 +1,107 @@
+package elysium
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"time"
+)
+
+// AuthSession is responsible for authentication and session management with the pantheon API.
+type AuthSession struct {
+	Token   string `json:"machine_token"`
+	Email   string `json:"email,omitempty"`
+	Client  string `json:"client"`
+	Expires int    `json:"expires_at"`
+	Session string `json:"session,omitempty"`
+	UserId  string `json:"user_id,omitempty"`
+}
+
+// NewAuthSession creates a new authentication session using a given token. It is the preferred method for creating a new AuthSession.
+func NewAuthSession(token string) *AuthSession {
+	return &AuthSession{
+		Token:  token,
+		Client: "terminus",
+	}
+}
+
+// Auth checks the expiration time of the current session (if there is one) and re-authenticates as necessary.
+func (a *AuthSession) Auth() error {
+	now := time.Now().UTC().Unix()
+	// Determine if an expiration has happened.
+	if int64(a.Expires) <= now {
+
+		// If so, re-authenticate.
+		json, err := a.JSON()
+		if err != nil {
+			return err
+		}
+
+		bytes, err := httpRequest("POST", a.Path("POST"), json, make(map[string]string))
+		if err != nil {
+			return err
+		}
+
+		return a.Unmarshal(bytes)
+	}
+	return nil
+}
+
+// Headers returns authentication headers needed for accessing the pantheon API. It should be used on all authenticated requests.
+func (a *AuthSession) Headers() (map[string]string, error) {
+	err := a.Auth()
+	if err != nil {
+		return nil, err
+	}
+
+	return map[string]string{"Authorization": fmt.Sprintf("Bearer %s", a.Session)}, nil
+}
+
+// Path returns the API endpoint for authenticating.
+func (a *AuthSession) Path(method string) string {
+	return "authorize/machine-token"
+}
+
+// JSON prepares the AuthSession struct for an HTTP request by stripping out fields which should not be sent, and returning the JSON representation of the struct as byte array.
+func (a AuthSession) JSON() ([]byte, error) {
+	a.Email = ""
+	a.UserId = ""
+	a.Session = ""
+	a.Expires = 0
+	return json.Marshal(a)
+}
+
+// Unmarshal converts the byte array from an HTTP response back into the AuthSession struct.
+func (a *AuthSession) Unmarshal(data []byte) error {
+	return json.Unmarshal(data, a)
+}
+
+// GetUser returns the UserID for the current AuthSession.
+func (a *AuthSession) GetUser() (string, error) {
+	err := a.Auth()
+	if err != nil {
+		return "", err
+	}
+
+	return a.UserId, nil
+}
+
+// Write converts the current AuthSession to JSON and writes persists result to the specified location.
+func (a *AuthSession) Write(location string) error {
+	contents, err := json.Marshal(a)
+	if err != nil {
+		return err
+	}
+
+	return ioutil.WriteFile(location, contents, 0644)
+}
+
+// Read loads the JSON representation of the AuthSession from the specified location.
+func (a *AuthSession) Read(location string) error {
+	bytes, err := ioutil.ReadFile(location)
+	if err != nil {
+		return err
+	}
+
+	return a.Unmarshal(bytes)
+}
---
+++ pkg/api/session_test.go
@@ -0,0 +1,55 @@
+package elysium
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/aws/aws-sdk-go/aws/awsutil"
+	"github.com/mitchellh/go-homedir"
+	"github.com/stretchr/testify/assert"
+)
+
+var session *AuthSession
+var sessionFilePath string
+
+func TestMain(m *testing.M) {
+	setup()
+	os.Exit(m.Run())
+}
+
+func setup() {
+	session = NewAuthSession(os.Getenv("DRUD_TERMINUS_TOKEN"), "beeradb@gmail.com")
+	home, err := homedir.Dir()
+	if err != nil {
+		log.Fatal("could not determine home directory")
+	}
+
+	// Try to read from a saved session, if we can.
+	sessionFilePath = filepath.Join(home, ".ddev", "pantheonapi")
+	err = session.Read(sessionFilePath)
+
+	if err != nil {
+		// If we can't load a session, try to auth directly.
+		err = session.Auth()
+		if err != nil {
+			log.Fatalf("Could not authentication: %v", err)
+		}
+
+		session.Write(sessionFilePath)
+	}
+}
+
+func TestAuthSession(t *testing.T) {
+	assert := assert.New(t)
+
+	req := Request{
+		Auth: session,
+	}
+
+	SiteList := &SiteList{}
+	err := req.Do("GET", SiteList)
+	assert.NoError(err)
+}
